bd302c01fc2463b7c88a657e48661d9162766437
ol4juwon <olajuwonlawal2012@gmail.com>
2022-09-27 00:06:15 +0100

    eslinter and state

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('Mario');
+  const handleClick = (e) => {
+    e.preventDefault();
+    setName('Luigi');
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {name}
+      <button onClick={handleClick}>Change name</button>
     </div>
   );
 }
